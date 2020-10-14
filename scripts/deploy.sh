821fb53a4a674da219dfc1cbdc0cb185fc0db874
Nehal Kanetkar <nkanetka@gmail.com>
2020-10-13 21:22:55 -0400

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+
+yarn run build
+
+date="$(date '+%Y%m%d%H%M')"
+
+cd dist
+git add -A
+git commit -m `Build ${date}`
+git push -f git@github.com:nkanetka/Daily-Report.git main:gh-pages
+cd -
