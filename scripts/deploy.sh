5d624c07228786c5ce0aa0695d4b3f6e8422d1e1
Nehal Kanetkar <nkanetka@gmail.com>
2020-10-13 21:29:12 -0400

    Fix commit message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ date="$(date '+%Y%m%d%H%M')"
 
 cd dist
 git add -A
-git commit -m `Build ${date}`
+git commit -m "`Build ${date}`"
 git push -f git@github.com:nkanetka/Daily-Report.git main:gh-pages
 cd -
